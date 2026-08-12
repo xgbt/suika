51d0b8a0d7cb62748b4864b586749cca0e788b05
zhuangqifeng <zhuangqifeng@xiaoyouzi.com>
2026-08-12 11:27:13 +0800

    refactor: drop recorder.rooms config in favor of sqlite

--- internal/conf/conf.pb.go
+++ internal/conf/conf.pb.go
@@ -82,14 +82,13 @@ func (x *Bootstrap) GetRecorder() *Recorder {
 	return nil
 }
 
-// Recorder configures the Bilibili live recorder: which rooms to watch,
-// where recordings land, and how recording/danmaku/reconnect behave.
+// Recorder configures the Bilibili live recorder: where recordings land
+// and how recording/danmaku/reconnect behave. Rooms to watch live in the
+// sqlite database (rooms table), not in config files.
 // Sensitive credentials (cookie) belong in configs/credentials.yaml,
 // which is merged into Bootstrap by the config file source and gitignored.
 type Recorder struct {
 	state protoimpl.MessageState `protogen:"open.v1"`
-	// Rooms to monitor.
-	Rooms []*Recorder_Room `protobuf:"bytes,1,rep,name=rooms,proto3" json:"rooms,omitempty"`
 	// Full cookie header including SESSDATA, required for source quality.
 	// Keep in configs/credentials.yaml (gitignored), never in config.yaml.
 	Cookie string `protobuf:"bytes,2,opt,name=cookie,proto3" json:"cookie,omitempty"`
@@ -146,13 +145,6 @@ func (x *Recorder) ProtoReflect() protoreflect.Message {
 // Deprecated: Use Recorder.ProtoReflect.Descriptor instead.
 func (*Recorder) Descriptor() ([]byte, []int) {
 	return file_conf_conf_proto_rawDescGZIP(), []int{1}
-}
-
-func (x *Recorder) GetRooms() []*Recorder_Room {
-	if x != nil {
-		return x.Rooms
-	}
-	return nil
 }
 
 func (x *Recorder) GetCookie() string {
@@ -322,71 +314,6 @@ func (x *Data) GetRedis() *Data_Redis {
 	return nil
 }
 
-// Room is one monitored live room.
-type Recorder_Room struct {
-	state protoimpl.MessageState `protogen:"open.v1"`
-	// Bilibili room id.
-	RoomId int64 `protobuf:"varint,1,opt,name=room_id,json=roomId,proto3" json:"room_id,omitempty"`
-	// Streamer display name, used in the recording directory name.
-	// When empty it is fetched from the room API on first contact.
-	Name string `protobuf:"bytes,2,opt,name=name,proto3" json:"name,omitempty"`
-	// Whether this room is monitored.
-	Enabled       bool `protobuf:"varint,3,opt,name=enabled,proto3" json:"enabled,omitempty"`
-	unknownFields protoimpl.UnknownFields
-	sizeCache     protoimpl.SizeCache
-}
-
-func (x *Recorder_Room) Reset() {
-	*x = Recorder_Room{}
-	mi := &file_conf_conf_proto_msgTypes[4]
-	ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-	ms.StoreMessageInfo(mi)
-}
-
-func (x *Recorder_Room) String() string {
-	return protoimpl.X.MessageStringOf(x)
-}
-
-func (*Recorder_Room) ProtoMessage() {}
-
-func (x *Recorder_Room) ProtoReflect() protoreflect.Message {
-	mi := &file_conf_conf_proto_msgTypes[4]
-	if x != nil {
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		if ms.LoadMessageInfo() == nil {
-			ms.StoreMessageInfo(mi)
-		}
-		return ms
-	}
-	return mi.MessageOf(x)
-}
-
-// Deprecated: Use Recorder_Room.ProtoReflect.Descriptor instead.
-func (*Recorder_Room) Descriptor() ([]byte, []int) {
-	return file_conf_conf_proto_rawDescGZIP(), []int{1, 0}
-}
-
-func (x *Recorder_Room) GetRoomId() int64 {
-	if x != nil {
-		return x.RoomId
-	}
-	return 0
-}
-
-func (x *Recorder_Room) GetName() string {
-	if x != nil {
-		return x.Name
-	}
-	return ""
-}
-
-func (x *Recorder_Room) GetEnabled() bool {
-	if x != nil {
-		return x.Enabled
-	}
-	return false
-}
-
 // DanmakuOptions tune danmaku event recording.
 type Recorder_DanmakuOptions struct {
 	state protoimpl.MessageState `protogen:"open.v1"`
@@ -399,7 +326,7 @@ type Recorder_DanmakuOptions struct {
 
 func (x *Recorder_DanmakuOptions) Reset() {
 	*x = Recorder_DanmakuOptions{}
-	mi := &file_conf_conf_proto_msgTypes[5]
+	mi := &file_conf_conf_proto_msgTypes[4]
 	ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 	ms.StoreMessageInfo(mi)
 }
@@ -411,7 +338,7 @@ func (x *Recorder_DanmakuOptions) String() string {
 func (*Recorder_DanmakuOptions) ProtoMessage() {}
 
 func (x *Recorder_DanmakuOptions) ProtoReflect() protoreflect.Message {
-	mi := &file_conf_conf_proto_msgTypes[5]
+	mi := &file_conf_conf_proto_msgTypes[4]
 	if x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -424,7 +351,7 @@ func (x *Recorder_DanmakuOptions) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use Recorder_DanmakuOptions.ProtoReflect.Descriptor instead.
 func (*Recorder_DanmakuOptions) Descriptor() ([]byte, []int) {
-	return file_conf_conf_proto_rawDescGZIP(), []int{1, 1}
+	return file_conf_conf_proto_rawDescGZIP(), []int{1, 0}
 }
 
 func (x *Recorder_DanmakuOptions) GetRecordInteractWord() bool {
@@ -462,7 +389,7 @@ type Recorder_ReconnectOptions struct {
 
 func (x *Recorder_ReconnectOptions) Reset() {
 	*x = Recorder_ReconnectOptions{}
-	mi := &file_conf_conf_proto_msgTypes[6]
+	mi := &file_conf_conf_proto_msgTypes[5]
 	ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 	ms.StoreMessageInfo(mi)
 }
@@ -474,7 +401,7 @@ func (x *Recorder_ReconnectOptions) String() string {
 func (*Recorder_ReconnectOptions) ProtoMessage() {}
 
 func (x *Recorder_ReconnectOptions) ProtoReflect() protoreflect.Message {
-	mi := &file_conf_conf_proto_msgTypes[6]
+	mi := &file_conf_conf_proto_msgTypes[5]
 	if x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -487,7 +414,7 @@ func (x *Recorder_ReconnectOptions) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use Recorder_ReconnectOptions.ProtoReflect.Descriptor instead.
 func (*Recorder_ReconnectOptions) Descriptor() ([]byte, []int) {
-	return file_conf_conf_proto_rawDescGZIP(), []int{1, 2}
+	return file_conf_conf_proto_rawDescGZIP(), []int{1, 1}
 }
 
 func (x *Recorder_ReconnectOptions) GetAutoReconnect() bool {
@@ -543,7 +470,7 @@ type Server_HTTP struct {
 
 func (x *Server_HTTP) Reset() {
 	*x = Server_HTTP{}
-	mi := &file_conf_conf_proto_msgTypes[7]
+	mi := &file_conf_conf_proto_msgTypes[6]
 	ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 	ms.StoreMessageInfo(mi)
 }
@@ -555,7 +482,7 @@ func (x *Server_HTTP) String() string {
 func (*Server_HTTP) ProtoMessage() {}
 
 func (x *Server_HTTP) ProtoReflect() protoreflect.Message {
-	mi := &file_conf_conf_proto_msgTypes[7]
+	mi := &file_conf_conf_proto_msgTypes[6]
 	if x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -603,7 +530,7 @@ type Server_GRPC struct {
 
 func (x *Server_GRPC) Reset() {
 	*x = Server_GRPC{}
-	mi := &file_conf_conf_proto_msgTypes[8]
+	mi := &file_conf_conf_proto_msgTypes[7]
 	ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 	ms.StoreMessageInfo(mi)
 }
@@ -615,7 +542,7 @@ func (x *Server_GRPC) String() string {
 func (*Server_GRPC) ProtoMessage() {}
 
 func (x *Server_GRPC) ProtoReflect() protoreflect.Message {
-	mi := &file_conf_conf_proto_msgTypes[8]
+	mi := &file_conf_conf_proto_msgTypes[7]
 	if x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -662,7 +589,7 @@ type Data_Database struct {
 
 func (x *Data_Database) Reset() {
 	*x = Data_Database{}
-	mi := &file_conf_conf_proto_msgTypes[9]
+	mi := &file_conf_conf_proto_msgTypes[8]
 	ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 	ms.StoreMessageInfo(mi)
 }
@@ -674,7 +601,7 @@ func (x *Data_Database) String() string {
 func (*Data_Database) ProtoMessage() {}
 
 func (x *Data_Database) ProtoReflect() protoreflect.Message {
-	mi := &file_conf_conf_proto_msgTypes[9]
+	mi := &file_conf_conf_proto_msgTypes[8]
 	if x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -716,7 +643,7 @@ type Data_Redis struct {
 
 func (x *Data_Redis) Reset() {
 	*x = Data_Redis{}
-	mi := &file_conf_conf_proto_msgTypes[10]
+	mi := &file_conf_conf_proto_msgTypes[9]
 	ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 	ms.StoreMessageInfo(mi)
 }
@@ -728,7 +655,7 @@ func (x *Data_Redis) String() string {
 func (*Data_Redis) ProtoMessage() {}
 
 func (x *Data_Redis) ProtoReflect() protoreflect.Message {
-	mi := &file_conf_conf_proto_msgTypes[10]
+	mi := &file_conf_conf_proto_msgTypes[9]
 	if x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -781,9 +708,8 @@ const file_conf_conf_proto_rawDesc = "" +
 	"\tBootstrap\x12*\n" +
 	"\x06server\x18\x01 \x01(\v2\x12.kratos.api.ServerR\x06server\x12$\n" +
 	"\x04data\x18\x02 \x01(\v2\x10.kratos.api.DataR\x04data\x120\n" +
-	"\brecorder\x18\x03 \x01(\v2\x14.kratos.api.RecorderR\brecorder\"\x97\b\n" +
-	"\bRecorder\x12/\n" +
-	"\x05rooms\x18\x01 \x03(\v2\x19.kratos.api.Recorder.RoomR\x05rooms\x12\x16\n" +
+	"\brecorder\x18\x03 \x01(\v2\x14.kratos.api.RecorderR\brecorder\"\x97\a\n" +
+	"\bRecorder\x12\x16\n" +
 	"\x06cookie\x18\x02 \x01(\tR\x06cookie\x12\x1f\n" +
 	"\vrecord_root\x18\x03 \x01(\tR\n" +
 	"recordRoot\x12O\n" +
@@ -795,11 +721,7 @@ const file_conf_conf_proto_rawDesc = "" +
 	"\rremux_enabled\x18\b \x01(\bH\x01R\fremuxEnabled\x88\x01\x01\x12=\n" +
 	"\adanmaku\x18\t \x01(\v2#.kratos.api.Recorder.DanmakuOptionsR\adanmaku\x12C\n" +
 	"\treconnect\x18\n" +
-	" \x01(\v2%.kratos.api.Recorder.ReconnectOptionsR\treconnect\x1aM\n" +
-	"\x04Room\x12\x17\n" +
-	"\aroom_id\x18\x01 \x01(\x03R\x06roomId\x12\x12\n" +
-	"\x04name\x18\x02 \x01(\tR\x04name\x12\x18\n" +
-	"\aenabled\x18\x03 \x01(\bR\aenabled\x1aB\n" +
+	" \x01(\v2%.kratos.api.Recorder.ReconnectOptionsR\treconnect\x1aB\n" +
 	"\x0eDanmakuOptions\x120\n" +
 	"\x14record_interact_word\x18\x01 \x01(\bR\x12recordInteractWord\x1a\xf4\x02\n" +
 	"\x10ReconnectOptions\x12*\n" +
@@ -847,44 +769,42 @@ func file_conf_conf_proto_rawDescGZIP() []byte {
 	return file_conf_conf_proto_rawDescData
 }
 
-var file_conf_conf_proto_msgTypes = make([]protoimpl.MessageInfo, 11)
+var file_conf_conf_proto_msgTypes = make([]protoimpl.MessageInfo, 10)
 var file_conf_conf_proto_goTypes = []any{
 	(*Bootstrap)(nil),                 // 0: kratos.api.Bootstrap
 	(*Recorder)(nil),                  // 1: kratos.api.Recorder
 	(*Server)(nil),                    // 2: kratos.api.Server
 	(*Data)(nil),                      // 3: kratos.api.Data
-	(*Recorder_Room)(nil),             // 4: kratos.api.Recorder.Room
-	(*Recorder_DanmakuOptions)(nil),   // 5: kratos.api.Recorder.DanmakuOptions
-	(*Recorder_ReconnectOptions)(nil), // 6: kratos.api.Recorder.ReconnectOptions
-	(*Server_HTTP)(nil),               // 7: kratos.api.Server.HTTP
-	(*Server_GRPC)(nil),               // 8: kratos.api.Server.GRPC
-	(*Data_Database)(nil),             // 9: kratos.api.Data.Database
-	(*Data_Redis)(nil),                // 10: kratos.api.Data.Redis
-	(*durationpb.Duration)(nil),       // 11: google.protobuf.Duration
+	(*Recorder_DanmakuOptions)(nil),   // 4: kratos.api.Recorder.DanmakuOptions
+	(*Recorder_ReconnectOptions)(nil), // 5: kratos.api.Recorder.ReconnectOptions
+	(*Server_HTTP)(nil),               // 6: kratos.api.Server.HTTP
+	(*Server_GRPC)(nil),               // 7: kratos.api.Server.GRPC
+	(*Data_Database)(nil),             // 8: kratos.api.Data.Database
+	(*Data_Redis)(nil),                // 9: kratos.api.Data.Redis
+	(*durationpb.Duration)(nil),       // 10: google.protobuf.Duration
 }
 var file_conf_conf_proto_depIdxs = []int32{
 	2,  // 0: kratos.api.Bootstrap.server:type_name -> kratos.api.Server
 	3,  // 1: kratos.api.Bootstrap.data:type_name -> kratos.api.Data
 	1,  // 2: kratos.api.Bootstrap.recorder:type_name -> kratos.api.Recorder
-	4,  // 3: kratos.api.Recorder.rooms:type_name -> kratos.api.Recorder.Room
-	11, // 4: kratos.api.Recorder.fallback_poll_interval:type_name -> google.protobuf.Duration
-	5,  // 5: kratos.api.Recorder.danmaku:type_name -> kratos.api.Recorder.DanmakuOptions
-	6,  // 6: kratos.api.Recorder.reconnect:type_name -> kratos.api.Recorder.ReconnectOptions
-	7,  // 7: kratos.api.Server.http:type_name -> kratos.api.Server.HTTP
-	8,  // 8: kratos.api.Server.grpc:type_name -> kratos.api.Server.GRPC
-	9,  // 9: kratos.api.Data.database:type_name -> kratos.api.Data.Database
-	10, // 10: kratos.api.Data.redis:type_name -> kratos.api.Data.Redis
-	11, // 11: kratos.api.Recorder.ReconnectOptions.reconnect_delay:type_name -> google.protobuf.Duration
-	11, // 12: kratos.api.Recorder.ReconnectOptions.health_check_interval:type_name -> google.protobuf.Duration
-	11, // 13: kratos.api.Server.HTTP.timeout:type_name -> google.protobuf.Duration
-	11, // 14: kratos.api.Server.GRPC.timeout:type_name -> google.protobuf.Duration
-	11, // 15: kratos.api.Data.Redis.read_timeout:type_name -> google.protobuf.Duration
-	11, // 16: kratos.api.Data.Redis.write_timeout:type_name -> google.protobuf.Duration
-	17, // [17:17] is the sub-list for method output_type
-	17, // [17:17] is the sub-list for method input_type
-	17, // [17:17] is the sub-list for extension type_name
-	17, // [17:17] is the sub-list for extension extendee
-	0,  // [0:17] is the sub-list for field type_name
+	10, // 3: kratos.api.Recorder.fallback_poll_interval:type_name -> google.protobuf.Duration
+	4,  // 4: kratos.api.Recorder.danmaku:type_name -> kratos.api.Recorder.DanmakuOptions
+	5,  // 5: kratos.api.Recorder.reconnect:type_name -> kratos.api.Recorder.ReconnectOptions
+	6,  // 6: kratos.api.Server.http:type_name -> kratos.api.Server.HTTP
+	7,  // 7: kratos.api.Server.grpc:type_name -> kratos.api.Server.GRPC
+	8,  // 8: kratos.api.Data.database:type_name -> kratos.api.Data.Database
+	9,  // 9: kratos.api.Data.redis:type_name -> kratos.api.Data.Redis
+	10, // 10: kratos.api.Recorder.ReconnectOptions.reconnect_delay:type_name -> google.protobuf.Duration
+	10, // 11: kratos.api.Recorder.ReconnectOptions.health_check_interval:type_name -> google.protobuf.Duration
+	10, // 12: kratos.api.Server.HTTP.timeout:type_name -> google.protobuf.Duration
+	10, // 13: kratos.api.Server.GRPC.timeout:type_name -> google.protobuf.Duration
+	10, // 14: kratos.api.Data.Redis.read_timeout:type_name -> google.protobuf.Duration
+	10, // 15: kratos.api.Data.Redis.write_timeout:type_name -> google.protobuf.Duration
+	16, // [16:16] is the sub-list for method output_type
+	16, // [16:16] is the sub-list for method input_type
+	16, // [16:16] is the sub-list for extension type_name
+	16, // [16:16] is the sub-list for extension extendee
+	0,  // [0:16] is the sub-list for field type_name
 }
 
 func init() { file_conf_conf_proto_init() }
@@ -893,14 +813,14 @@ func file_conf_conf_proto_init() {
 		return
 	}
 	file_conf_conf_proto_msgTypes[1].OneofWrappers = []any{}
-	file_conf_conf_proto_msgTypes[6].OneofWrappers = []any{}
+	file_conf_conf_proto_msgTypes[5].OneofWrappers = []any{}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: unsafe.Slice(unsafe.StringData(file_conf_conf_proto_rawDesc), len(file_conf_conf_proto_rawDesc)),
 			NumEnums:      0,
-			NumMessages:   11,
+			NumMessages:   10,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
